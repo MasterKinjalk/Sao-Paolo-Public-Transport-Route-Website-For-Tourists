37a115f5bd517217fedda5d7710769bf2c0a6cbe
S Ajay <71289526+legendslayer01@users.noreply.github.com>
2023-11-18 20:06:25 -0600

    🛂 added basic auth and routes

--- front-end/transport/src/App.js
+++ front-end/transport/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, Link, Navigate } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import LoginButton from './Auth/Login.jsx';
+import LogoutButton from './Auth/Logout.jsx';
+import Profile from './Auth/Profile.jsx';
 
 function App() {
+  const { isLoading, isAuthenticated } = useAuth0();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Transport</p>
+      <nav>
+        {isAuthenticated && <Link to="/profile">Profile</Link>}
+        {/* {isAuthenticated && <Link to="/logout">Logout</Link>} */}
+        {isAuthenticated && <LogoutButton />}
+      </nav>
+
+      <Routes>
+        <Route
+          path="/profile"
+          element={isAuthenticated ? <Profile /> : <Navigate to="/" />}
+        />
+        {/* <Route path="/logout" element={<LogoutButton />} /> */}
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? <Navigate to="/profile" /> : <LoginButton />
+          }
+        />
+      </Routes>
     </div>
   );
 }
--- front-end/transport/src/index.js
+++ front-end/transport/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { BrowserRouter as Router } from 'react-router-dom';
+const root = createRoot(document.getElementById('root'));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
