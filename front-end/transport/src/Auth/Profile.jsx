2beb4dac9c8b5514abf82a29a114e8176c76da96
S Ajay <71289526+legendslayer01@users.noreply.github.com>
2023-11-21 13:27:18 -0600

    🚧 profile state

--- front-end/transport/src/Auth/Profile.jsx
+++ front-end/transport/src/Auth/Profile.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { CircularProgress, Typography, Avatar, Box } from '@mui/material';
+import { useEffect } from 'react';
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
+  useEffect(() => {
+    console.log(user);
+    console.log(isAuthenticated);
+  }, [isAuthenticated]);
 
+  // if (isAuthenticated) {
+  //   console.log(user);
+  // }
   if (isLoading) {
     return <CircularProgress />;
-  }
-
-  if (isAuthenticated) {
-    console.log(user);
   }
 
   return (
