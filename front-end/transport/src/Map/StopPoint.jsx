fc8e0e1939f514a0e74b3927399e5d710ae2e4da
S Ajay <71289526+legendslayer01@users.noreply.github.com>
2023-11-21 22:01:47 -0600

    pointers

--- front-end/transport/src/Map/StopPoint.jsx
+++ front-end/transport/src/Map/StopPoint.jsx
@@ -1,3 +1,77 @@
+// import 'mapbox-gl/dist/mapbox-gl.css';
+// import * as React from 'react';
+// import Map, { Marker, FullscreenControl } from 'react-map-gl';
+// import { Box } from '@mui/material';
+// import { styled } from '@mui/system';
+
+// const MapContainer = styled(Box)({
+//   position: 'relative',
+//   width: '100%',
+//   height: '100vh',
+// });
+
+// const FullscreenControlContainer = styled('div')({
+//   position: 'absolute',
+//   top: 20,
+//   left: 20,
+// });
+
+// const MapWithMarkers = ({ stopsData, centers }) => {
+//   const [viewState, setViewState] = React.useState({
+//     zoom: 3.5,
+//     // longitude: centers[0],
+//     // latitude: centers[1],
+//   });
+
+//   console.log(stopsData);
+//   const f100 = stopsData.slice(0, 100);
+
+//   console.log('f100');
+//   console.log(f100);
+
+//   // for (let i = 0; i < f100.length; i++) {
+//   //   console.log(f100[i]);
+//   // }
+//   const maxBounds = [-52.8267, -14.9927, -46.3565, -23.3565];
+
+//   return (
+//     <MapContainer>
+//       <Map
+//         {...viewState}
+//         mapStyle="mapbox://styles/mapbox/streets-v12"
+//         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+//         onMove={(evt) => setViewState(evt.viewState)}
+//         // maxBounds={maxBounds}
+//       >
+//         {f100.map((stop, index) => (
+//           <Marker
+//             key={index}
+//             latitude={parseFloat(stop[1])}
+//             longitude={parseFloat(stop[2])}
+//           >
+//             <div
+//               style={{
+//                 color: 'white',
+//                 background: 'blue',
+//                 borderRadius: '50%',
+//                 padding: '2px',
+//               }}
+//             >
+//               {stop[0]}
+//             </div>
+//           </Marker>
+//         ))}
+
+//         <FullscreenControlContainer>
+//           <FullscreenControl />
+//         </FullscreenControlContainer>
+//       </Map>
+//     </MapContainer>
+//   );
+// };
+
+// export default MapWithMarkers;
+
 import 'mapbox-gl/dist/mapbox-gl.css';
 import * as React from 'react';
 import Map, { Marker, FullscreenControl } from 'react-map-gl';
@@ -23,8 +97,10 @@ const MapWithMarkers = ({ stopsData, centers }) => {
     // latitude: centers[1],
   });
 
+  const [hoveredStop, setHoveredStop] = React.useState(null);
+
   console.log(stopsData);
-  const f100 = stopsData.slice(0, 1);
+  const f100 = stopsData.slice(0, 100);
 
   console.log('f100');
   console.log(f100);
@@ -52,12 +128,38 @@ const MapWithMarkers = ({ stopsData, centers }) => {
             <div
               style={{
                 color: 'white',
-                background: 'blue',
-                borderRadius: '50%',
-                padding: '2px',
+                background: 'red',
+                borderRadius: '4px',
+                padding: '5px',
+                display: hoveredStop === stop[0] ? 'block' : 'none',
+                position: 'absolute',
+                top: '-50px',
+                left: '-15px',
+                zIndex: 9999,
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                maxWidth: '120px',
+                textAlign: 'center',
               }}
             >
               {stop[0]}
+            </div>
+            <div
+              onMouseEnter={() => setHoveredStop(stop[0])}
+              onMouseLeave={() => setHoveredStop(null)}
+              style={{ zIndex: 1 }}
+            >
+              {/* Your marker design */}
+              <div
+                style={{
+                  width: '10px',
+                  height: '10px',
+                  background: 'blue',
+                  borderRadius: '50%',
+                  cursor: 'pointer',
+                }}
+              ></div>
             </div>
           </Marker>
         ))}
@@ -67,79 +169,7 @@ const MapWithMarkers = ({ stopsData, centers }) => {
         </FullscreenControlContainer>
       </Map>
     </MapContainer>
-    // <div>
-    //   {stopsData.map((stop, index) => (
-    //     <div key={index}>
-    //       {index < 100 ? (
-    //         <div>
-    //           {stop[0]} {stop[1]} {stop[2]}
-    //         </div>
-    //       ) : (
-    //         ''
-    //       )}
-    //       {/* {stop[0]} {stop[1]} {stop[2]} */}
-    //     </div>
-    //   ))}
-    // </div>
   );
 };
 
 export default MapWithMarkers;
-
-// import 'mapbox-gl/dist/mapbox-gl.css';
-// import * as React from 'react';
-// // import ReactMapGL, Map,{ Source, Layer, FullscreenControl } from 'react-map-gl';
-// import Map, { Source, Layer, FullscreenControl } from 'react-map-gl';
-// import { Box } from '@mui/material';
-// import { styled } from '@mui/system';
-
-// const MapContainer = styled(Box)({
-//   position: 'relative',
-//   width: '100%',
-//   height: '100vh',
-// });
-
-// const FullscreenControlContainer = styled('div')({
-//   position: 'absolute',
-//   top: 20,
-//   left: 20,
-// });
-
-// const MapWithPolygon = ({ polygonData, centers }) => {
-//   const [viewport, setViewport] = React.useState({
-//     width: '100%',
-//     height: '100%',
-//     latitude: centers[0],
-//     longitude: centers[1],
-//     zoom: 3.5,
-//   });
-
-//   return (
-//     <MapContainer>
-//       <Map
-//         {...viewport}
-//         mapStyle="mapbox://styles/mapbox/streets-v12"
-//         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-//         onViewportChange={(newViewport) => setViewport(newViewport)}
-//       >
-//         {/* Render the GeoJSON line */}
-//         <Source id="line" type="geojson" data={polygonData}>
-//           <Layer
-//             id="outline"
-//             type="line"
-//             paint={{
-//               'line-color': '#000',
-//               'line-width': 3,
-//             }}
-//           />
-//         </Source>
-
-//         <FullscreenControlContainer>
-//           <FullscreenControl />
-//         </FullscreenControlContainer>
-//       </Map>
-//     </MapContainer>
-//   );
-// };
-
-// export default MapWithPolygon;
