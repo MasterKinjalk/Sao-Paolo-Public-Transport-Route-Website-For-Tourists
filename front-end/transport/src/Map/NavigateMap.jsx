daf9b5d777d8f9ad9040a5a190862e1378f627e4
S Ajay <71289526+legendslayer01@users.noreply.github.com>
2023-11-26 19:37:24 -0600

    ✨ copy lon lat

--- front-end/transport/src/Map/NavigateMap.jsx
+++ front-end/transport/src/Map/NavigateMap.jsx
@@ -1,11 +1,11 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
 import * as React from 'react';
 import Map, { FullscreenControl } from 'react-map-gl';
-import { Box } from '@mui/material';
+import { Box, IconButton, Tooltip } from '@mui/material';
 import { styled } from '@mui/system';
 import mapboxgl from 'mapbox-gl';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
-
+import FileCopyIcon from '@mui/icons-material/FileCopy';
 const MapContainer = styled(Box)({
   position: 'relative',
   width: '100%',
@@ -16,6 +16,14 @@ const FullscreenControlContainer = styled('div')({
   position: 'absolute',
   top: 20,
   left: 20,
+});
+const CopyIconContainer = styled('div')({
+  position: 'absolute',
+  top: 50,
+  right: 20,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px',
 });
 
 const MapC = () => {
@@ -30,7 +38,7 @@ const MapC = () => {
   // Function to check and retrieve coordinates from local storage
   React.useEffect(() => {
     const getCoordinatesFromLocalStorage = () => {
-      const Coordinatesv = localStorage.getItem('startCoordinate');
+      const Coordinatesv = localStorage.getItem('coordinates');
 
       if (Coordinatesv) {
         setCoordinate(JSON.parse(Coordinatesv));
@@ -40,16 +48,39 @@ const MapC = () => {
     getCoordinatesFromLocalStorage();
   }, []);
 
+  // const geocodePlace = async (place) => {
+  //   try {
+  //     const response = await fetch(
+  //       `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?access_token=${process.env.REACT_APP_MAPBOX}`
+  //     );
+  //     const data = await response.json();
+  //     const coordinates = data.features[0].geometry.coordinates;
+  //     return coordinates;
+  //   } catch (error) {
+  //     console.error('Error geocoding place: ', error);
+  //     return null;
+  //   }
+  // };
+
   // const maxBounds = [-46.8267, -23.9927, -46.3565, -23.3565];
 
-  const addDirections = (map) => {
+  const addDirections = async (map) => {
+    // const p1 = await geocodePlace(Coordinate[0][0]);
+    // const p2 = await geocodePlace(Coordinate[1][0]);
+    // const p1_lat = Coordinate[0][1];
+    // const p1_lon = Coordinate[0][2];
+    // const p2_lat = Coordinate[1][1];
+    // const p2_lon = Coordinate[1][2];
+    const [coord1, coord2] = Coordinate;
+    console.log(coord1, coord2);
+
     const directions = new MapboxDirections({
       accessToken: process.env.REACT_APP_MAPBOX,
       unit: 'metric',
-      waypoints:
-        Coordinate && Coordinate.length === 2
-          ? [{ coordinates: Coordinate[0] }, { coordinates: Coordinate[1] }]
-          : undefined,
+
+      // Coordinate && Coordinate.length === 2
+      //   ? [{ coordinates: p1 }, { coordinates: p2 }]
+      //   : undefined,
     });
 
     map.addControl(directions, 'top-left');
@@ -57,6 +88,26 @@ const MapC = () => {
 
   const handleMapLoad = (map) => {
     addDirections(map);
+  };
+
+  const handleCopyClick = (coordinateIndex) => {
+    const [coord1, coord2] = Coordinate;
+    const coordinate = coordinateIndex === 1 ? coord1 : coord2;
+    console.log(coordinate[2], coordinate[1]);
+    const textToCopy = `${coordinate[2]}, ${coordinate[1]}`;
+
+    navigator.clipboard
+      .writeText(textToCopy)
+      .then(() => {
+        console.log(
+          `Copied ${
+            coordinateIndex === 1 ? 'p1' : 'p2'
+          } coordinates to clipboard`
+        );
+      })
+      .catch((err) => {
+        console.error('Failed to copy: ', err);
+      });
   };
 
   return (
@@ -69,6 +120,19 @@ const MapC = () => {
         // maxBounds={maxBounds}
         onLoad={(event) => handleMapLoad(event.target)}
       >
+        <CopyIconContainer>
+          <Tooltip title="Copy p1 coordinates" placement="left">
+            <IconButton onClick={() => handleCopyClick(1)}>
+              <FileCopyIcon />1
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Copy p2 coordinates" placement="left">
+            <IconButton onClick={() => handleCopyClick(2)}>
+              <FileCopyIcon />2
+            </IconButton>
+          </Tooltip>
+        </CopyIconContainer>
+
         <FullscreenControlContainer>
           <FullscreenControl />
         </FullscreenControlContainer>
